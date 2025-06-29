6877bfdd98ac160e01b7d72571072b53440d5082
Vilde Hagtvedt <54098897+vildeHagt@users.noreply.github.com>
2025-06-29 21:13:45 +0200

    fix

--- scripts/atlasDog.js
+++ scripts/atlasDog.js
@@ -3,7 +3,7 @@
 
 const ATLAS_IMAGE_URL = chrome.runtime.getURL("images/atlasWalk.gif"); // Use your dog gif
 const ATLAS_ID = "atlas-dog-walker";
-const WALK_DURATION = 8000; // ms for dog to cross the screen
+const WALK_DURATION = 10000; // ms for dog to cross the screen
 const INTERVAL_MINUTES = 3; // Change to your desired interval
 
 function injectAtlasDog() {
@@ -12,10 +12,10 @@ function injectAtlasDog() {
   img.src = ATLAS_IMAGE_URL;
   img.id = ATLAS_ID;
   img.style.position = "fixed";
-  img.style.bottom = "20px";
+  img.style.bottom = "1px";
   img.style.right = "-150px";
   img.style.left = "";
-  img.style.height = "80px";
+  img.style.height = "60px";
   img.style.zIndex = 99999;
   img.style.transition = `right ${WALK_DURATION}ms linear`;
   img.style.transform = "scaleX(-1)"; // Flip image to face left
